7fceb747ef9cdf7cd58c7ca54e13627903fba57e
Theo <theo.paschoa@ua.pt>
2025-04-29 18:03:18 +0100

    persistencia perfil

--- app/src/main/java/com/example/pecimobileapp/ui/screens/DefineWorkoutScreen.kt
+++ app/src/main/java/com/example/pecimobileapp/ui/screens/DefineWorkoutScreen.kt
@@ -9,7 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.example.pecimobileapp.ui.*
+import com.example.pecimobileapp.viewmodels.ProfileViewModel
 
 @Composable
 fun DefineWorkoutScreen(navController: NavController) {
